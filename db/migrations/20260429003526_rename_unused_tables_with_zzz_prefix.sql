2866e0c340b10b2276b41970a76dd7d29757cbbe
seq030 <218389549+seq030@users.noreply.github.com>
2026-04-29 00:40:43 +0900

    refactor: rename unused tables

---
+++ db/migrations/20260429003526_rename_unused_tables_with_zzz_prefix.sql
@@ -0,0 +1,7 @@
+alter table parties rename to zzz_parties;
+alter table content_memos rename to zzz_content_memos;
+alter table content_comments rename to zzz_content_comments;
+alter table student_gradings rename to zzz_student_gradings;
+alter table student_grading_tags rename to zzz_student_grading_tags;
+alter table feedback_submissions rename to zzz_feedback_submissions;
+alter table user_activities rename to zzz_user_activities;
